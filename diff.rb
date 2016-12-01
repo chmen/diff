c2649a2f8bed6614d0eccbcd4e40bed3cf4db71a
Chmen <tony.potapov@gmail.com>
2016-12-01 12:00:27 +0200

    modify get_text method to return array

--- diff.rb
+++ diff.rb
@@ -4,20 +4,8 @@ class Diff
 
   end
 
-  def self.sync(first_path, second_path)
-    i = 1
-    File.open(first_path, 'r') do |f1|
-      f1.each_line do |line|
-      	puts line
-        File.open(second_path, 'r') do |f2|
-          f2.each_line do |line2|
-          	puts line2
-            return i if line.eql?(line2)
-          end
-        end
-        i += 1
-      end
-    end
+  def self.sync(text1, text2)
+
   end
 
   def numerate
@@ -29,15 +17,11 @@ class Diff
   end
 
   def self.get_text(text_path)
-    text = ''
+    text = []
     File.open(text_path, 'r') do |f|
-      f.each_line {|line| text += line}
+      f.each_line {|line| text.push(line.gsub("\n", ''))}
     end
     text
-  end
-
-  def self.test
-    "Hello World"
   end
 end
 
--- spec/diff_spec.rb
+++ spec/diff_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe Diff do
-  describe '.test' do
-    it 'return hello world' do
-      expect(Diff.test).to eq "Hello World"
-    end
-  end
 
   # describe '.sync' do
   #   it 'return number of line, that common for both files' do
@@ -15,10 +10,7 @@ describe Diff do
 
   describe '.get_text' do
     it 'return text form file' do
-      expect(Diff.get_text('1.txt')).to eq "Some
-Simple
-Text 
-File"
+      expect(Diff.get_text('1.txt')).to eq ['Some', 'Simple', 'Text', 'File']
     end
   end
 
