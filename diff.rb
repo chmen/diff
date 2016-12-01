c473360b358779d6fd492e293df65b5fa15847ce
Chmen <tony.potapov@gmail.com>
2016-12-01 10:25:06 +0200

    create simple test for app

---
+++ diff.rb
@@ -0,0 +1,17 @@
+
+
+def diff(first_path, second_path)
+ 
+end
+
+puts "Real output"
+puts diff('1.txt', '2.txt')
+
+puts "Expect output:"
+puts "1     	*	Some|Another
+2	-	Simple
+3		Text
+4		File
+5	+	With
+6	+	Additional
+7	+	Lines"
