fe000b41ab2c59eb33c9e4991dd8c08c35903b3c
Chmen <tony.potapov@gmail.com>
2016-12-01 15:00:55 +0200

    add combine method with spec

--- diff.rb
+++ diff.rb
@@ -4,16 +4,55 @@ class Diff
 
   end
 
-  def self.sync(text1, text2)
+  def self.sync(array1, array2)
+    array1.each do |a1|
+      array2.each do |a2|
+        #return array1.index(a1) if a1 == a2 && Diff.second_match(array1.index(array1, array2, a1, a2)
+      end
+    end
+  end
 
+  def self.match(array1, array2)
+    array1.each do |a1|
+      array2.each do |a2|
+        return [array1.index(a1), array2.index(a2)] if a1 == a2
+      end
+    end
   end
 
   def numerate
 
   end
 
-  def mark
+  def mark(array1, result)
+    
+  end
 
+  def self.combine(array1, array2, match_indexes)
+    result = array1
+    m1 = match_indexes[0]
+    m2 = match_indexes[1]
+
+    (m2..(array2.size - 1)).each do |i|
+      if result[m1] == nil
+        result.push(array2[i])
+      elsif array2[i] != result[m1]
+      	result[m1] += "|#{array2[i]}"
+      end
+      m1 += 1
+    end
+    
+    m1 = match_indexes[0]
+    (0..m2).each do |i|
+      
+      if result[m1] == nil
+        result.push(array2[m2 - i])
+      elsif array2[m2-i] != result[m1]
+        result[m1] += "|#{array2[m2 - i]}"
+      end
+      m1 -= 1
+    end
+    result
   end
 
   def self.get_text(text_path)
@@ -24,4 +63,3 @@ class Diff
     text
   end
 end
-
--- spec/diff_spec.rb
+++ spec/diff_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe Diff do
 
-  # describe '.sync' do
-  #   it 'return number of line, that common for both files' do
-  #     expect(Diff.sync('1.txt', '2.txt')).to eq 2
-  #   end
-  # end
+   # describe '.sync' do
+   #   it 'return number of line, that common for both files' do
+   #     array1 = ['Some', 'Simple', 'Text', 'File']
+   #     array2 = ['Text', 'Text', 'Text', 'Text']
+   #     expect(Diff.sync(array1, array2)).to eq 2
+   #   end
+   # end
 
   describe '.get_text' do
     it 'return text form file' do
@@ -14,4 +16,24 @@ describe Diff do
     end
   end
 
+  describe '.match' do 
+    it 'return array with two match indexes' do
+      array1 = ['Some', 'Simple', 'Text', 'File']
+      array2 = ['Another', 'Text', 'File', 'With', 'Additional', 'Lines']
+      expect(Diff.match(array1, array2)).to eq [2, 1]
+    end
+  end
+
+  describe '.combine' do
+    it 'return combine array from 2 arrays' do
+      array1 = ['Some', 'Simple', 'Text', 'File']
+      array2 = ['Another', 'Text', 'File', 'With', 'Additional', 'Lines']
+      match_indexes = [2, 1]
+      array3 = ['Some', 'Simple|Another', 'Text', 'File', 'With', 'Additional', 'Lines']
+      expect(Diff.combine(array1, array2, match_indexes)).to eq array3
+    end
+  end
+
+  describe '.mark' do
+  end
 end
